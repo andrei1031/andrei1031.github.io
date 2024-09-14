357c5fecdcc40be2d0989533248c886056c19e6c
andrei1031 <145421529+andrei1031@users.noreply.github.com>
2024-09-14 21:48:23 +0800

    Update map.js

--- map.js
+++ map.js
@@ -7,4 +7,4 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var marker = L.marker([16.384114750000002,120.59481175]).addTo(mymap);
 
 
-marker.bindPopup("<b>Hello!</b><br>This is where i live").openPopup();
+marker.bindPopup("<b>Hello!</b><br>This is where i live!").openPopup();
